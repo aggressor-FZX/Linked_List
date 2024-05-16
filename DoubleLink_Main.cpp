90d4c5f6f7d392ffd8d486ac357f5e741a3b6477
JD Calderon <jeff.d.calderon@gmail.com>
2024-05-16 16:57:46 -0700

    fixed comparison to string operator in jcstring for seeker to work. can now return ptr to item in list based on str argument

--- DoubleLink_Main.cpp
+++ DoubleLink_Main.cpp
@@ -10,12 +10,18 @@ int main()
 	DoubleLinkedList dll;
 	JCString jcstr1("Hello");
 	JCString jcstr2("World");
+	JCString jcstr3("World");
+	bool compare = jcstr1 == jcstr2.c_str();
+	cout << " compare result hello to world " << compare << endl;
+	bool compare2 = jcstr2 == jcstr3.c_str();
+	cout << " compare result world to world " << compare2 << endl;
+
 	JCString jcstr3("Goodbye");
 	JCString jcstr4("Cruel");
 	JCString jcstr5("World");
 
-	dll.push_back(jcstr1);
-	dll.push_back(jcstr2);
+	dll.insert(jcstr1);
+	dll.insert(jcstr2);
 	dll.push_back(jcstr3);
 	dll.push_back(jcstr4);
 	dll.push_back(jcstr5);
@@ -23,6 +29,7 @@ int main()
 	cout << dll << endl;
 
 	cout << "Count: " << dll.getCount() << endl;
+
 	dll.remove(jcstr2);
 	cout << dll << endl;
 
--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -50,7 +50,7 @@ ostream& operator<<(ostream& inputStrm, const DoubleLinkedList& dll)
 // destructor
 DoubleLinkedList::~DoubleLinkedList()
 {
-	cout << "in linkedlist destructor" << endl;
+	cout << "in linked list destructor" << endl;
 
 	// Changing to "current" for clarity
 	JCNode* current = this->head; // creat pointer to head
@@ -92,39 +92,97 @@ bool DoubleLinkedList::hasMore() const
 }
 bool DoubleLinkedList::push_back(const JCString& str)
 {
-	//create new node
 
 	if (this->count == 0)
 	{
+		// fist node, create new head node
 		this->initHeadNode(str);
-		++this->count;
 
 		return true;
 
 	}
-	else 
+	else if(this->count > 0)
 	{
-		JCNode* newNode =  new JCNode(str); 
-		this->head->next = newNode;//old head's 'next' points to new head node
-		newNode->prev = this->head; // New head node's previous points to old head node;
+		JCNode* newNode;
+		newNode = new JCNode(str);
+
+		this->head->next = newNode; //old head's 'next' points to new head node
+		newNode->prev = this->head; // New head node's previous points to old head node;A
+		newNode->next = nullptr; // New head node's next points to nullptr
 		this->head = newNode;//place on top of stack
 		 
 		++this->count;
+
+		return true;
+
+	} else {
+		return false;
 	}
-	return true;
+
+	return false;
 }
+
 bool DoubleLinkedList::remove(const JCString& str)
 {
 	return false;
 }
+
+// Smaller letters come first hence A < B < C etc. list starts small goes large
 bool DoubleLinkedList::insert(const JCString& str)
 {
-	//create new node
-	JCNode* newNode =  new JCNode(str); 
-	//compare 
+	bool success = false;
 
+	// Case where list is empty
+	if (this->count == 0)
+	{
+		// Place on top of list and return true
+		this->initHeadNode(str);
+
+		success = true;
+		
+	}// Case where this is only one item in list
+	else if(this->count == 1)
+	{
+		
+		this->it = this->head;
+		
+		if (this->it->data == str)//using JCString to string compare operator
+		{
+			// Already in list, return false
+			success = false;
+		}
+		else if (this->it->data > str)// Arg String comes before 
+		{
+			//string arg becomes new head
+			// call push back to handle it and increment this->count
+			success = this->push_back(str);
+		}
+		else if (this->it->data < str) // Arg String comes after
+		{
+			// Create new node with string arg
+			JCNode* newNode = new JCNode(str);
+
+			// Place node behind the head
+			this->it->prev = newNode; // point prev to  this node
+			newNode->next = this->head; // newNode next points to the head
+			newNode->prev = nullptr; // only two items in list
+
+			// New node is bottom of list
+			this->tail = newNode;
+
+			// Increment list
+			++this->count;
+
+			// Reset iter
+			this->resetIteration();
+			success = true;
+		}
+
+	}
 	
+	return success;
 }
+
 void DoubleLinkedList::initHeadNode(const JCString& str)
 {
 	JCNode* newNode =  new JCNode(str); 
@@ -132,5 +190,28 @@ void DoubleLinkedList::initHeadNode(const JCString& str)
 	this->tail = newNode;
 	newNode->next = nullptr;
 	newNode->prev = nullptr;
+	++this->count;
 
+}
+
+//	Free standing helper function
+JCNode* seeker(const char* str, const DoubleLinkedList* dll)
+{	
+	char* checkString = nullptr; //looking for this string
+
+	dll->it = dll->head; // start at the head node
+
+	while (dll->it != nullptr) //go until we prev ptr is null
+	{	
+		if(dll->it->data == str) //if we find it 
+		{
+			return dll->it; //return it ptr
+		}
+		else 
+		{//move backwards
+			dll->it = dll->it->prev;
+		}
+	}
+
+	return nullptr; //didn't find it return null
 }
